1576377fd7ca7f67a16bc4190840eb793292efb0
Jacek Gizinski <jacektrg+github@gmail.com>
2024-07-30 19:53:29 +0200

    Setting up redirects.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	redirects() {
+		return [
+			{
+				source: '/room',
+				destination: '/',
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig;
