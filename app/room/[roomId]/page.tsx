62e10afd5d2c1ffc9bc583593deb43bbf5452e7c
Jacek Gizinski <jacektrg+github@gmail.com>
2024-09-24 20:41:43 +0200

    Adding state for searchResults videos.
    Updating websocket events.
    Adding function handling searching for video.
    Adding function handling playing, stopping, seeking, changing, ending video.
    Updating passed down props to Navbar, Playlist and ReactPlayer components.

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -4,17 +4,29 @@ import styles from './room.module.scss';
 import { useEffect, useState } from 'react';
 import Navbar from '@/app/components/Navbar';
 import Loading from '@/app/components/Loading';
-import { ChatData, SocketType, UserData, VideoData } from '@/app/libs/types';
+import {
+	ChatData,
+	Hosts,
+	SocketType,
+	UserData,
+	VideoData,
+} from '@/app/libs/types';
 import Chat from '@/app/components/Chat';
 import Playlist from '@/app/components/Playlist';
 import ReactPlayer from 'react-player';
 import { useParams, useRouter } from 'next/navigation';
 import { io } from 'socket.io-client';
+import {
+	extractVideoId,
+	formatFetchError,
+	getVideoData,
+} from '@/app/libs/utils';
 
 export default function Room() {
 	const { roomId }: { roomId: string } = useParams();
 	const [fetching, setFetching] = useState<boolean>(false);
 	const [error, setError] = useState<string | null>(null);
+	const [searchResults, setSearchResults] = useState<VideoData[] | null>(null);
 	const [videoData, setVideoData] = useState<VideoData[]>([]);
 	const [showNavbar, setShowNavbar] = useState<boolean>(true);
 	const [showChat, setShowChat] = useState<boolean>(true);
@@ -25,6 +37,9 @@ export default function Room() {
 		messageList: [],
 		videoList: [],
 	});
+	const [videoPlaying, setVideoPlaying] = useState<boolean>(false);
+	const [videoPlaybackTime, setVideoPlaybackTime] = useState<number>(0);
+
 	const router = useRouter();
 
 	useEffect(() => {
@@ -34,6 +49,9 @@ export default function Room() {
 	}, [roomId, router]);
 
 	useEffect(() => {
+		if (!roomId) {
+			return;
+		}
 		const newSocket = io(`${process.env.NEXT_PUBLIC_API_URI}/rooms`, {
 			query: { roomId },
 			withCredentials: true,
@@ -52,30 +70,39 @@ export default function Room() {
 		socket.on('oops', (error) => {
 			console.error(error);
 		});
-		socket.on('all_data', (data) => {
-			setUserData(data.userData);
+		socket.on('all_room_data', (roomData) => {
+			setUserData(roomData.userData);
 			setChatData({
-				userList: data.userList,
-				messageList: data.messageList,
-				videoList: data.videoList,
+				userList: roomData.userList,
+				messageList: roomData.messageList,
+				videoList: roomData.videoList,
 			});
 		});
-		socket.on('new_user', (data) => {
+		socket.on('new_user_joined', (userData) => {
 			setChatData((prevState) => ({
-				userList: [...prevState.userList, data],
+				userList: [...prevState.userList, userData],
 				messageList: prevState.messageList,
 				videoList: prevState.videoList,
 			}));
 		});
-		socket.on('new_message', (data) => {
+		socket.on('new_message_received', (messageData) => {
 			setChatData((prevState) => ({
 				userList: prevState.userList,
-				messageList: [...prevState.messageList, data],
+				messageList: [...prevState.messageList, messageData],
 				videoList: prevState.videoList,
 			}));
 		});
-		socket.on('new_video', (data) => {
-			setVideoData(data);
+		socket.on('new_video_added', (videoData) => {
+			setVideoData(videoData);
+		});
+		socket.on('start_video_playback', () => {
+			setVideoPlaying(true);
+		});
+		socket.on('stop_video_playback', () => {
+			setVideoPlaying(false);
+		});
+		socket.on('jump_to_video_time', (data) => {
+			setVideoPlaybackTime(data.time);
 		});
 
 		return () => {
@@ -86,14 +113,72 @@ export default function Room() {
 	function handleSendMessage(message: string) {
 		if (!socket || !roomId) return;
 		socket.emit('send_message', {
-			roomId: roomId as string,
+			roomId: roomId,
 			message: message,
 		});
 	}
 
-	function handleAddVideo(video: string) {
+	async function handleSearchVideo(host: Hosts, string: string) {
+		try {
+			if (!host || !string) return;
+			setFetching(true);
+			const id = extractVideoId(host, string);
+			if (!id) {
+				setError('Unsupported host or incorrect Id');
+				return;
+			}
+			const data = await getVideoData(host, id);
+			setSearchResults(data);
+			setFetching(false);
+		} catch (error) {
+			setError(
+				formatFetchError(error).statusText ||
+					'Unknown fetching error. Make sure you selected correct source.'
+			);
+			setFetching(false);
+		}
+	}
+
+	function handleAddVideo(video: VideoData) {
 		if (!socket || !video) return;
 		socket.emit('add_video', { roomId, video });
+	}
+
+	function handlePlayVideo() {
+		if (!socket || !roomId) return;
+		socket.emit('start_video_playback', {
+			roomId: roomId,
+		});
+	}
+
+	function handleStopVideo() {
+		if (!socket || !roomId) return;
+		socket.emit('stop_video_playback', {
+			roomId: roomId,
+		});
+	}
+
+	function handleVideoSeek(time: number) {
+		if (!socket || !roomId || !time) return;
+		socket.emit('jump_to_video_time', {
+			roomId: roomId,
+			time: time,
+		});
+	}
+
+	function handleVideoChange(videoId: string) {
+		if (!socket || !roomId || !videoId) return;
+		socket.emit('change_video', {
+			roomId: roomId,
+			videoId: videoId,
+		});
+	}
+
+	function handleVideoEnded() {
+		if (!socket || !roomId) return;
+		socket.emit('video_ended', {
+			roomId: roomId,
+		});
 	}
 
 	function dismissError() {
@@ -111,6 +196,8 @@ export default function Room() {
 	return (
 		<>
 			<Navbar
+				searchVideo={handleSearchVideo}
+				searchResults={searchResults}
 				addVideo={handleAddVideo}
 				showNavbar={showNavbar}
 				toggleNavbar={toggleNavbarVisibility}
@@ -137,6 +224,13 @@ export default function Room() {
 								height={'100%'}
 								muted={true}
 								controls={true}
+								loop={false}
+								playing={videoPlaying}
+								onProgress={({ playedSeconds }) =>
+									setVideoPlaybackTime(playedSeconds)
+								}
+								onSeek={(seconds) => handleVideoSeek(seconds)}
+								onEnded={handleVideoEnded}
 							/>
 						)}
 					</div>
@@ -163,7 +257,11 @@ export default function Room() {
 							chatMessages={chatData.messageList}
 							sendMessage={handleSendMessage}
 						/>
-						<Playlist show={!showChat} playlist={videoData} />
+						<Playlist
+							show={!showChat}
+							playlist={videoData}
+							changeVideo={handleVideoChange}
+						/>
 					</div>
 				</div>
 			</main>
