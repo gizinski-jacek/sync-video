9ce0b2c4f1385d66c048e100e39c64ff82d2dcd8
Jacek Gizinski <jacektrg+github@gmail.com>
2024-09-24 20:28:50 +0200

    Adding youtube image hosting domains

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,15 @@ const nextConfig = {
 			},
 		];
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*ytimg.com',
+				port: '',
+			},
+		],
+	},
 };
 
 export default nextConfig;
