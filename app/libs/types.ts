bc03c509b45549fa8278419f7456cd021c89fd9b
Jacek Gizinski <jacektrg+github@gmail.com>
2024-09-24 20:34:37 +0200

    Adding new websocket events between client and server.
    Adding Youtube response type.
    Updating previous types.

--- app/libs/types.ts
+++ app/libs/types.ts
@@ -7,41 +7,100 @@ export interface UserData {
 	name: string;
 }
 
-export interface ChatMessage {
+export interface MessageData {
 	user: UserData;
 	message: string;
 	timestamp: string;
 }
 
-export interface VideoData {
-	id: string;
-	url: string;
-}
-
 export interface ChatData {
 	userList: UserData[];
-	messageList: ChatMessage[];
+	messageList: MessageData[];
 	videoList: VideoData[];
 }
 
 type ServerToClientEvents = {
 	oops: (error: any) => void;
-	all_data: (data: {
+	all_room_data: (roomData: {
 		userData: UserData;
 		userList: UserData[];
-		messageList: ChatMessage[];
+		messageList: MessageData[];
 		videoList: VideoData[];
 	}) => void;
-	new_user: (chatData: UserData) => void;
-	new_message: (message: {
+	new_user_joined: (userData: UserData) => void;
+	new_message_received: (messageData: {
 		user: UserData;
 		message: string;
 		timestamp: string;
 	}) => void;
-	new_video: (video: VideoData[]) => void;
+	new_video_added: (videoData: VideoData[]) => void;
+	start_video_playback: () => void;
+	stop_video_playback: () => void;
+	jump_to_video_time: (data: { time: number }) => void;
+	video_ended: (data: { roomId: string; time: number }) => void;
 };
 
 type ClientToServerEvents = {
 	send_message: (data: { roomId: string; message: string }) => void;
-	add_video: (data: { roomId: string; video: string }) => void;
+	add_video: (data: { roomId: string; video: VideoData }) => void;
+	start_video_playback: (data: { roomId: string }) => void;
+	stop_video_playback: (data: { roomId: string }) => void;
+	jump_to_video_time: (data: { roomId: string; time: number }) => void;
+	change_video: (data: { roomId: string; videoId: string }) => void;
+	video_ended: (data: { roomId: string }) => void;
 };
+
+export type Hosts = 'youtube';
+
+export interface VideoData {
+	host: Hosts;
+	id: string;
+	url: string;
+	title: string | null;
+	channelId: string | null;
+	channelName: string;
+	iFrameSrcId: string;
+	livestreamChat: boolean;
+	thumbnailUrl: string | null;
+}
+
+export interface YoutubeResponse {
+	kind: string;
+	etag: string;
+	nextPageToken: string;
+	prevPageToken: string;
+	pageInfo: {
+		totalResults: number;
+		resultsPerPage: number;
+	};
+	items: YoutubeVideo[];
+}
+
+export interface YoutubeVideo {
+	kind: string;
+	etag: string;
+	id: string;
+	snippet: {
+		publishedAt: string;
+		channelId: string;
+		title: string;
+		description: string;
+		thumbnails: {
+			[key: string]: {
+				url: string;
+				width: number;
+				height: number;
+			};
+		};
+		channelTitle: string;
+		tags: string[];
+		categoryId: string;
+		liveBroadcastContent: string;
+		defaultLanguage: string;
+		localized: {
+			title: string;
+			description: string;
+		};
+		defaultAudioLanguage: string;
+	};
+}
