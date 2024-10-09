4d6aec7ba38e3ffbf5c9991620b8a2fcabfc9a26
Jacek Gizinski <jacektrg+github@gmail.com>
2024-10-09 18:10:29 +0200

    Removing redundant container element

--- app/components/Playlist.tsx
+++ app/components/Playlist.tsx
@@ -9,59 +9,57 @@ interface Props {
 
 export default function Playlist({ playlist, removeVideo }: Props) {
 	return (
-		<div className={styles.playlist}>
-			<ul className={styles.container}>
-				{playlist.map((video, index) => (
-					<li key={video.id}>
-						<div className={styles.video}>
-							{video.thumbnailUrl ? (
-								<Image
-									src={video.thumbnailUrl}
-									width={130}
-									height={90}
-									alt={`${video.title} thumbnail` || 'Video thumbnail'}
-								/>
-							) : (
-								<div className={`${styles.placeholder} relative`} />
-							)}
-							<p className='flex-1 m-0'>{video.title}</p>
-							<div className='flex flex-col justify-between'>
-								<div
-									className='bg-red-700 cursor-pointer'
-									onClick={() => removeVideo(video)}
+		<ul className={styles.playlist}>
+			{playlist.map((video, index) => (
+				<li key={video.id}>
+					<div className={styles.video}>
+						{video.thumbnailUrl ? (
+							<Image
+								src={video.thumbnailUrl}
+								width={130}
+								height={90}
+								alt={`${video.title} thumbnail` || 'Video thumbnail'}
+							/>
+						) : (
+							<div className={`${styles.placeholder} relative`} />
+						)}
+						<p className='flex-1 m-0'>{video.title}</p>
+						<div className='flex flex-col justify-between'>
+							<div
+								className='bg-red-700 cursor-pointer'
+								onClick={() => removeVideo(video)}
+							>
+								<svg
+									width='24px'
+									viewBox='0 0 24 24'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
 								>
-									<svg
-										width='24px'
-										viewBox='0 0 24 24'
-										fill='none'
-										xmlns='http://www.w3.org/2000/svg'
-									>
-										<g strokeWidth='0'></g>
-										<g strokeLinecap='round' strokeLinejoin='round'></g>
-										<g>
-											<path
-												d='M7 17L16.8995 7.10051'
-												stroke='#000000'
-												strokeWidth='1.5'
-												strokeLinecap='round'
-												strokeLinejoin='round'
-											></path>
-											<path
-												d='M7 7.00001L16.8995 16.8995'
-												stroke='#000000'
-												strokeWidth='1.5'
-												strokeLinecap='round'
-												strokeLinejoin='round'
-											></path>
-										</g>
-									</svg>
-								</div>
+									<g strokeWidth='0'></g>
+									<g strokeLinecap='round' strokeLinejoin='round'></g>
+									<g>
+										<path
+											d='M7 17L16.8995 7.10051'
+											stroke='#000000'
+											strokeWidth='1.5'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+										></path>
+										<path
+											d='M7 7.00001L16.8995 16.8995'
+											stroke='#000000'
+											strokeWidth='1.5'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+										></path>
+									</g>
+								</svg>
 							</div>
 						</div>
-						{index !== playlist.length - 1 && <hr />}
-					</li>
-				))}
-			</ul>
-		</div>
+					</div>
+					{index !== playlist.length - 1 && <hr />}
+				</li>
+			))}
+		</ul>
 	);
 }
