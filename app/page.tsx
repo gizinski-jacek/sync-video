51504511c8a65d7a3380c1fa3f3ce48d0f2b98d1
Jacek Gizinski <jacektrg+github@gmail.com>
2024-11-18 20:56:30 +0100

    Adding error state for rendering Error component displaying error msg.

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,44 @@
 import axios, { AxiosResponse } from 'axios';
 import { useRouter } from 'next/navigation';
 import { formatFetchError } from './libs/utils';
+import { useState } from 'react';
+import Error from './components/Error';
 
 export default function App() {
 	const router = useRouter();
+	const [error, setError] = useState<string | null>(null);
 
 	async function handleCreateRoom(e: React.MouseEvent<HTMLButtonElement>) {
 		try {
 			e.preventDefault();
 			const res: AxiosResponse<{ roomId: string }> = await axios.get(
-				`${process.env.NEXT_PUBLIC_API_URI}/api/create-room2`,
+				`${process.env.NEXT_PUBLIC_API_URI}/api/create-room`,
 				{ timeout: 5000 }
 			);
 			router.push(`room/${res.data.roomId}`);
 		} catch (error: unknown) {
-			console.error(formatFetchError(error));
+			setError(formatFetchError(error).statusText);
 		}
 	}
 
+	function dismissError() {
+		setError(null);
+	}
+
 	return (
-		<div>
-			<button type='button' onClick={handleCreateRoom}>
+		<div className='min-h-[50%] flex items-center'>
+			<button
+				className='bg-lime-950 p-2 rounded text-3xl font-bold'
+				type='button'
+				onClick={handleCreateRoom}
+			>
 				Create Room
 			</button>
+			{error && (
+				<Error dismiss={dismissError} isAbsoluteAtTop={true}>
+					{error}
+				</Error>
+			)}
 		</div>
 	);
 }
