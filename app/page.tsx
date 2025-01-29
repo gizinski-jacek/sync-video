294648d9c1bf2e453be511a472675fee514727e7
Jacek Gizinski <jacektrg+github@gmail.com>
2025-01-29 21:14:49 +0100

    Adding fetching state to render Loading component.

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,17 @@ import { useRouter } from 'next/navigation';
 import { formatFetchError } from './libs/utils';
 import { useState } from 'react';
 import Error from './components/Error';
+import Loading from './components/Loading';
 
 export default function App() {
 	const router = useRouter();
 	const [error, setError] = useState<string | null>(null);
+	const [fetching, setFetching] = useState<boolean>(false);
 
 	async function handleCreateRoom(e: React.MouseEvent<HTMLButtonElement>) {
 		try {
 			e.preventDefault();
+			setFetching(true);
 			const API_URI =
 				process.env.NEXT_PUBLIC_NODE_ENV === 'production'
 					? process.env.NEXT_PUBLIC_API_URI
@@ -28,6 +31,7 @@ export default function App() {
 			router.push(`room/${res.data.roomId}`);
 		} catch (error: unknown) {
 			setError(formatFetchError(error).statusText);
+			setFetching(false);
 		}
 	}
 
@@ -36,14 +40,18 @@ export default function App() {
 	}
 
 	return (
-		<div className='min-h-[50%] flex items-center'>
+		<div className='min-h-[50%] m-auto flex flex-col items-center'>
 			<button
-				className='bg-lime-950 p-2 rounded text-3xl font-bold'
+				className={`bg-lime-950 p-2 rounded text-3xl font-bold ${
+					fetching ? 'brightness-75' : ''
+				}`}
 				type='button'
 				onClick={handleCreateRoom}
+				disabled={fetching}
 			>
 				Create Room
 			</button>
+			{fetching && <Loading styleClass='mx-auto w-[200px] h-[200px]' />}
 			{error && (
 				<Error dismiss={dismissError} isAbsoluteAtTop={true}>
 					{error}
